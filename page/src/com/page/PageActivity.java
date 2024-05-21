9b8eb9c61d6084bd4061eb0b7fc9737929bb162c
VinthKumar-S <137074055+VinthKumar-S@users.noreply.github.com>
2024-05-21 11:56:32 +0530

    ad5

---
+++ page/src/com/page/PageActivity.java
@@ -0,0 +1,18 @@
+package com.page;
+
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
+import android.os.Bundle;
+import android.view.*;
+
+public class PageActivity extends Activity{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+   
+	
+}
