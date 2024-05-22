37cbd9c400a0db40e70737e01ec183dbdf8bcec6
VinthKumar-S <137074055+VinthKumar-S@users.noreply.github.com>
2024-05-22 11:31:27 +0530

    ad4

---
+++ tab/src/com/tab/TabActivity.java
@@ -0,0 +1,18 @@
+package com.tab;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+import android.view.View.*;
+
+public class TabActivity extends Activity  {
+    /** Called when the activity is first created. */
+   
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+     
+    }
+	
+}
