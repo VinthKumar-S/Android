7556e4257bd5669d4954be3dfa16942909d9ca08
VinthKumar-S <137074055+VinthKumar-S@users.noreply.github.com>
2024-05-22 11:48:09 +0530

    ad2

--- access/gen/com/access/R.java
+++ access/gen/com/access/R.java
@@ -14,16 +14,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int c=0x7f050001;
-        public static final int d=0x7f050000;
+        public static final int c=0x7f050000;
+        public static final int d=0x7f050001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int Call=0x7f040002;
-        public static final int Dail=0x7f040001;
-        public static final int app_name=0x7f040003;
+        public static final int app_name=0x7f040001;
         public static final int hello=0x7f040000;
     }
 }
--- access/src/com/access/AccessActivity.java
+++ access/src/com/access/AccessActivity.java
@@ -1,32 +1,40 @@
 package com.access;
 
 import android.app.Activity;
-import android.content.*;
-import android.net.*;
-import android.view.*;
-import android.view.View.*;
-import android.widget.*;
 import android.os.Bundle;
+import android.widget.*;
+import android.view.*;
+import android.content.*;
+import android.view.View.*;
+import android.net.*;
 
 public class AccessActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
-	Button b1,b2;
+    Button c,d;
+    
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        b1.findViewById(R.id.c);
-        b2.findViewById(R.id.d);
-        b2.setOnClickListener(this);
-        b1.setOnClickListener(this);
+        c=(Button)findViewById(R.id.c);
+        d=(Button)findViewById(R.id.d);
+        c.setOnClickListener(this);
+        d.setOnClickListener(this);
     }
-    
+
+
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
-		if(v.getId()==R.id.d)
-		{
-		 Intent i=new Intent(android.content.Intent.ACTION_DIAL,Uri.parse("tel:+642624060"));
-		 startActivity(i);
+		if(v.getId()==R.id.d){
+			Intent i1=new Intent(android.content.Intent.ACTION_VIEW,Uri.parse("http://www.amazon.com"));
+			startActivity(i1);
 		}
-		
+	
+		if(v.getId()==R.id.c){
+			Intent i2=new Intent(android.content.Intent.ACTION_DIAL,Uri.parse("tel:+1233333"));
+			startActivity(i2);
+		}
+	
 	}
+
+
 }
