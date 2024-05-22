090798f7c1812d05544d73a0af529aba8ac1df58
VinthKumar-S <137074055+VinthKumar-S@users.noreply.github.com>
2024-05-22 11:32:30 +0530

    ad5

---
+++ toast/src/com/toast/ToastActivity.java
@@ -0,0 +1,29 @@
+package com.toast;
+
+
+
+import android.app.Activity;
+
+import android.os.Bundle;
+import android.view.*;
+import android.widget.*;
+import android.view.View.*;
+
+public class ToastActivity extends Activity implements OnClickListener {
+    /** Called when the activity is first created. */
+	 Button b;
+	    EditText e;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        b=(Button)findViewById(R.id.b2);
+        e=(EditText)findViewById(R.id.e1);
+        b.setOnClickListener(this);
+    }
+    @Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		String s1="Hello "+e.getText();
+		Toast.makeText(getApplicationContext(),s1,Toast.LENGTH_SHORT).show();
+	}
+}
