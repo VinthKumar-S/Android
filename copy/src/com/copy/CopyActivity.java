58b9a6d70bd1143f06e8d85a117d342e4ccb93a2
VinthKumar-S <137074055+VinthKumar-S@users.noreply.github.com>
2024-05-23 20:44:51 +0530

    ad1

---
+++ copy/src/com/copy/CopyActivity.java
@@ -0,0 +1,13 @@
+package com.copy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CopyActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ cycle/src/com/cycle/CycleActivity.java
@@ -0,0 +1,16 @@
+package com.cycle;
+
+import android.app.Activity;
+import android.util.*;
+import android.os.Bundle;
+
+public class CycleActivity extends Activity {
+    /** Called when the activity is first created. */
+    
+    private static final String MES="MyMessage"; 
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+      
+    }
+    
+}
